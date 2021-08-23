9d233dcb717b5058b93be329c47cbdcc5f7c4cdc
Dhenlok <lhsabino18@gmail.com>
2021-08-23 14:19:52 -0300

    Exercício 12

--- semana3/revisao-js/lista-revisao-js/exercicios.js
+++ semana3/revisao-js/lista-revisao-js/exercicios.js
@@ -107,7 +107,11 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-    
+    novoObjeto = {
+        ...pessoa,
+        nome: "ANÔNIMO"
+    }
+    return novoObjeto
 }
 
 // EXERCÍCIO 13A
